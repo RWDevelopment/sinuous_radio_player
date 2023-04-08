2ad014d1fdf697393b559d2225104be5be51b47a
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2023-04-08 13:56:16 +0200

    Update stations.js

--- stations/stations.js
+++ stations/stations.js
@@ -3,27 +3,27 @@ const stations = [
     {
         'category': 'jazz',
         'name': 'Radio Swiss Jazz',
-        'stream': 'http://stream.srg-ssr.ch/m/rsj/aacp_96',
+        'stream': '//stream.srg-ssr.ch/m/rsj/aacp_96',
     },
     {
         'category': 'jazz funk',
         'name': 'Secret Agent',
-        'stream': 'http://ice1.somafm.com/secretagent-128-mp3',
+        'stream': '//ice1.somafm.com/secretagent-128-mp3',
     },
     {
         'category': 'jazz funk',
         'name': 'NRK Jazz',
-        'stream': 'http://lyd.nrk.no/nrk_radio_jazz_mp3_h',
+        'stream': '//lyd.nrk.no/nrk_radio_jazz_mp3_h',
     },
     {
         'category': 'jazz',
         'name': 'The Pittsburgh Jazz Channel',
-        'stream': 'https://pubmusic.streamguys1.com/wzum-aac',
+        'stream': '//pubmusic.streamguys1.com/wzum-aac',
     },
     {
         'category': 'jazz',
         'name': '1000 Jazzhits',
-        'stream': 'https://1000jazzhits.stream.laut.fm/1000jazzhits',
+        'stream': '//1000jazzhits.stream.laut.fm/1000jazzhits',
     }
 
 ];
