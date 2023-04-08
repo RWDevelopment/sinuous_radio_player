57dd2c347e3b7253d0d3dcf0ca3b7722d50c37a0
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2023-04-08 13:13:00 +0200

    Update stations.js

--- stations/stations.js
+++ stations/stations.js
@@ -1,11 +1,6 @@
 const stations = [
 
     {
-        'category': 'funk',
-        'name': 'Swiss Groove',
-        'stream': 'http://158.69.74.203:80/;stream/1',
-    },
-    {
         'category': 'jazz',
         'name': 'Radio Swiss Jazz',
         'stream': 'http://stream.srg-ssr.ch/m/rsj/aacp_96',
@@ -21,21 +16,6 @@ const stations = [
         'stream': 'http://lyd.nrk.no/nrk_radio_jazz_mp3_h',
     },
     {
-        'category': 'funk',
-        'name': 'Amsterdam Funk Channel',
-        'stream': 'http://213.202.241.199:9292/;',
-    },
-    {
-        'category': 'funk',
-        'name': 'Funky Corner Radio',
-        'stream': 'http://176.31.111.65:4744/;',
-    },
-    {
-        'category': 'jazz',
-        'name': 'Radio Sky Music Jazz',
-        'stream': 'http://sr3.publicssl.net:7006/live',
-    },
-    {
         'category': 'jazz',
         'name': 'The Pittsburgh Jazz Channel',
         'stream': 'https://pubmusic.streamguys1.com/wzum-aac',
@@ -44,11 +24,6 @@ const stations = [
         'category': 'jazz',
         'name': '1000 Jazzhits',
         'stream': 'https://1000jazzhits.stream.laut.fm/1000jazzhits',
-    },
-    {
-        'category': 'electronic',
-        'name': 'Synthetic FM',
-        'stream': 'http://stream.syntheticfm.com:8040/live'
     }
 
 ];
