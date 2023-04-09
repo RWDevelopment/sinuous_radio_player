d681cb929f68c9b18a505244366f95cccd3f4c60
JakWeg <33756545+RWDevelopment@users.noreply.github.com>
2023-04-09 15:06:23 +0200

    Add files via upload

--- components/play_animation.js
+++ components/play_animation.js
@@ -1,13 +1,7 @@
 import { html } from 'sinuous';
 
 const PlayAnim = () => {
-    return html`
-    <li class="playing">
-        <span class="playing__bar playing__bar1"></span>
-        <span class="playing__bar playing__bar2"></span>
-        <span class="playing__bar playing__bar3"></span>
-    </li>
-    `;
+    return html`<li class="playing"><span class="playing__bar playing__bar1"></span><span class="playing__bar playing__bar2"></span><span class="playing__bar playing__bar3"></span></li>`;
 }
 
 export default PlayAnim;
